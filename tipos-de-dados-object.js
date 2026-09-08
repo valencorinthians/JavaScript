70efebc6b57e2a1dfec76dde2be85ab2d2b07532
Valentina Alves <valencorinthians@gmail.com>
2026-09-08 18:24:29 -0300

    Update tipos-de-dados-object.js

---
+++ tipos-de-dados-object.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome: "Valentina",
+    idade: 28,
+    casado: true,
+};
+// Acessando propriedade do objeto
+
+console.log(pessoa.nome); // "Valentina"
+console.log(pessoa.idade);  // 28
