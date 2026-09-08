1204a5436404dfd987525c94964914eeeba7769f
Valentina Alves <valencorinthians@gmail.com>
2026-09-08 17:56:28 -0300

    add array

---
+++ tipos-de-dados-array.js
@@ -0,0 +1,8 @@
+let fruta = ["maça", "banana", "laranja"]; // Array de strings
+let numeros = [1, 2, 3, 4, 5]; // Array de números
+let misto = [true, "true", 42]; // Array com tipos de dado
+
+console.log (fruta)
+console.log (numeros)
+console.log (misto)
+
