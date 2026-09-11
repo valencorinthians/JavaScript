97e084651f05558b549668ec1dea3302ca691b0d
Valentina Alves <valencorinthians@gmail.com>
2026-09-11 17:33:26 -0300

    add operadoratruibuicaomodulo JavaScript

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,6 @@
+let totalDoces = 10;
+let criancas = 3;
+
+// Queremos saber quantos doces sobram após dividir igualmente
+totalDoces%= criancas;
+console.log(totalDoces); // 1 -> Poisa 10 dividido por 3 sobra 1
